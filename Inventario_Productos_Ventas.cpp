18ff1ed070b5cf2bdf0370e972219e227baa70d5
Samuel-119052 <samusmusic18@gmail.com>
2025-06-28 23:51:10 -0500

    Funcionalidad añadida: Buscar producto

--- Inventario_Productos_Ventas.cpp
+++ Inventario_Productos_Ventas.cpp
@@ -40,6 +40,27 @@ void listaProductos(){
 		}
 	}
 }
+void buscarProducto(){
+	if(Cntpro==0){
+		cout<<"No hay productos registrados"<<endl;
+	}else{
+		cin.ignore();
+		string Buspro;
+		cout<<"Ingrese el nombre del producto a buscar"<<endl;
+		getline(cin, Buspro);
+		bool Enc = false;
+		for(int i=0; i<Cntpro; i++){
+			if(Pro[i].Nompr == Buspro){
+				cout<<"Producto encontrado: "<<Pro[i].Nompr<<endl;
+				cout<<"Precio: "<<Pro[i].Prcp<<"$"<<endl;
+				Enc=true;
+			}
+		}
+		if(Enc==false){
+			cout<<"Producto no encontrado"<<endl;
+		}
+	}
+}
 void menu(){
 	char op;
 	while(op!='9'){
@@ -47,12 +68,15 @@ void menu(){
 		cout<<"Inventario de productos y ventas"<<endl;
 		cout<<"1) Registrar un nuevo producto"<<endl;
 		cout<<"2) Listar Productos"<<endl;
+		cout<<"3) Buscar producto"<<endl;
 		cout<<"Elija una opcion"<<endl;
 		cin>>op;
 		if(op=='1'){
 			registrarProducto();
 		}else if(op=='2'){
 			listaProductos();
+		}else if(op=='3'){
+			buscarProducto();
 		}
 	}
 }
